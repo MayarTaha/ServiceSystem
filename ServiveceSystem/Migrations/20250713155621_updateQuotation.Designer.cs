9328027938bb04ee90e07c64275633131b1a7dbd
mayar <yorataha112@gmail.com>
2025-07-13 18:03:11 +0200

    service

--- ServiveceSystem/Migrations/20250713155621_updateQuotation.Designer.cs
+++ ServiveceSystem/Migrations/20250713155621_updateQuotation.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ServiveceSystem.Models;
 
@@ -11,9 +12,11 @@ using ServiveceSystem.Models;
 namespace ServiveceSystem.Migrations
 {
     [DbContext(typeof(AppDBContext))]
-    partial class AppDBContextModelSnapshot : ModelSnapshot
+    [Migration("20250713155621_updateQuotation")]
+    partial class updateQuotation
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
